fba8e8bfd40830230067b6f9bd5aa757f290c5bb
Aditya <adityawavnew@gmail.com>
2023-04-27 04:56:30 +0530

    made routes in mongoose

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://gofood:gofood@gofood.ekat4sc.mongodb.net/?retryWrites=true&w=majority'
-const mongoDB =() =>{
-    mongoose.connect(mongoURI, ()=>{
-        console.log("Connnected");
+const mongoURI = 'mongodb://gofood:gofood@ac-mmjquoc-shard-00-00.ekat4sc.mongodb.net:27017,ac-mmjquoc-shard-00-01.ekat4sc.mongodb.net:27017,ac-mmjquoc-shard-00-02.ekat4sc.mongodb.net:27017/gofoodmern?ssl=true&replicaSet=atlas-o8ganf-shard-0&authSource=admin&retryWrites=true&w=majority'
+const mongoDB = async() =>{
+   await mongoose.connect(mongoURI,{ useNewUrlParser: true },async(err,result)=>{
+        if(err) console.log("---",err)
+        else{
+            console.log("Connnected");
+            const fetched_data = await mongoose.connection.db.collection("food_items");
+            fetched_data.find({}).toArray(function( err, data){
+                if(err)
+                    console.log(err);
+                else
+                    console.log();
+            })
+        }
     });
 }
 
