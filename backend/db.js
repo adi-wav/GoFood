3870466f4b039c1a42b11ac3d2b9edb7f35140f7
Aditya Sharma <adityawavnew@gmail.com>
2023-06-01 21:27:24 +0530

    added cart checkout function

--- backend/db.js
+++ backend/db.js
@@ -1,36 +1,26 @@
-const mongoose = require("mongoose");
-const mongoURI =
-  "mongodb://gofood:gofood@ac-mmjquoc-shard-00-00.ekat4sc.mongodb.net:27017,ac-mmjquoc-shard-00-01.ekat4sc.mongodb.net:27017,ac-mmjquoc-shard-00-02.ekat4sc.mongodb.net:27017/gofoodmern?ssl=true&replicaSet=atlas-o8ganf-shard-0&authSource=admin&retryWrites=true&w=majority";
-const mongoDB = async () => {
-  await mongoose.connect(
-    mongoURI,
-    { useNewUrlParser: true },
-    async (err, result) => {
-      if (err) console.log("---", err);
-      else {
-        console.log("Connnected");
-        const fetched_data = await mongoose.connection.db.collection(
-          "food_items"
-        );
-        fetched_data.find({}).toArray(async function (err, data) {
-          const foodCategory = await mongoose.connection.db.collection(
-            "foodCategory"
-          );
-          foodCategory.find({}).toArray(function (err, catData) {
-            if (err) console.log(err);
-            else {
-                global.food_items = data;
-                global.foodCategory = catData;
-            }
-          })
-          // if(err)
-          //     console.log(err);
-          // else
-          //     global.food_items = data;
-        })
-      }
-    }
-  );
-};
+const mongoose = require('mongoose')
+// const mongoDbClient = require("mongodb").MongoClient
+const mongoURI = 'mongodb://<!#@!#@>:"!@!@"@merncluster-shard-00-00.d1d4z.mongodb.net:27017,merncluster-shard-00-01.d1d4z.mongodb.net:27017,merncluster-shard-00-02.d1d4z.mongodb.net:27017/Customer?ssl=true&replicaSet=atlas-eusy5p-shard-0&authSource=admin&retryWrites=true&w=majority' // Customer change url to your db you created in atlas
+// mongodb://<username>:<password>@merncluster-shard-00-00.d1d4z.mongodb.net:27017,merncluster-shard-00-01.d1d4z.mongodb.net:27017,merncluster-shard-00-02.d1d4z.mongodb.net:27017/?ssl=true&replicaSet=atlas-eusy5p-shard-0&authSource=admin&retryWrites=true&w=majority
+module.exports = function (callback) {
+    mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
+        // mongoDbClient.connect(mongoURI, { useNewUrlParser: true }, async(err, result) => {
+        if (err) console.log("---" + err)
+        else {
+            // var database =
+            console.log("connected to mongo")
+            const foodCollection = await mongoose.connection.db.collection("food_items");
+            foodCollection.find({}).toArray(async function (err, data) {
+                const categoryCollection = await mongoose.connection.db.collection("Categories");
+                categoryCollection.find({}).toArray(async function (err, Catdata) {
+                    callback(err, data, Catdata);
 
-module.exports = mongoDB;
+                })
+            });
+            // listCollections({name: 'food_items'}).toArray(function (err, database) {
+            // });
+            //     module.exports.Collection = database;
+            // });
+        }
+    })
+};
